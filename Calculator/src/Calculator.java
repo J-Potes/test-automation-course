250a37a43181d3fbcba0994cb7c06dce7c36b665
J-Potes <jpotes.multimedia@gmail.com>
2026-02-26 18:44:24 -0500

    Added sum and substract methods to Calculator class

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,2 +1,9 @@
 public class Calculator {
+    public double sum (number1, number2){
+        return number1 + number2;
+    }
+
+    public double substract (number1, number2){
+        return number1 - number2;
+    }
 }
